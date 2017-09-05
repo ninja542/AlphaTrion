f7d8163ef53d5b4e5ba0b4a689783d336247c5ed
ByrdOfAFeather <matthew_a_byrd@outlook.com>
2017-09-05 16:24:01 -0400

    Fixed static files

--- AlphaTrion/settings.py
+++ AlphaTrion/settings.py
@@ -138,3 +138,4 @@ X_FRAME_OPTIONS = 'DENY'
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
