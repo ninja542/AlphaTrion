8f9bae320fb6fde014b56011f9976b998616f486
ByrdOfAFeather <matthew_a_byrd@outlook.com>
2017-09-21 16:58:49 -0400

    Laying groundwork for future project page, fixed minor database issues and migrations.

--- Project/models.py
+++ Project/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 # Create your models here.
+
+class Questions(models.Model):
+	question = models.CharField(max_length=200)
+
+
+class CustomSurvey(models.Model):
+	title = models.CharField(max_length=150)
+	date = models.DateField()
+	author = models.ForeignKey(User)
+	questions = models.ManyToManyField(Questions, through='SurveyQuestions')
+
+class SurveyQuestions(models.Model):
+	survey = models.ForeignKey(CustomSurvey)
+	question = models.ForeignKey(Questions)
+
+
+
+
