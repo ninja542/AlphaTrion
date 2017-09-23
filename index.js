2e64f5f4c221cd2b0ea20275103bafe6c8172f21
ByrdOfAFeather <matthew_a_byrd@outlook.com>
2017-09-23 12:58:43 -0400

    Attempting more NodeJs installations

---
+++ index.js
@@ -0,0 +1,14 @@
+// Taken from https://github.com/heroku/node-js-sample/blob/master/index.js
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
