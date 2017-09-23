0feb0fde7aa30f4011ddc70c136ba288f107c32b
ByrdOfAFeather <matthew_a_byrd@outlook.com>
2017-09-23 13:18:56 -0400

    Fixed another function name conflict

--- Project/views.py
+++ Project/views.py
@@ -4,6 +4,9 @@ from .models import SenateProjects
 def projects_home(request):
 	return render(request, 'projects-home.html')
 
+def student_projects(request):
+	return render(request, 'student-projects.html')
+
 def senate_projects(request):
 	senate_projects = SenateProjects.objects.all()
 	return render(request, 'senate-projects.html', {'senate-projects': senate_projects})
